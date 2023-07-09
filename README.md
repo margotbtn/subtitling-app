62262e9c96d4311ed4edbaeed9b93ecf0c74c27a
margot BERTON <margot.berton@telecom-sudparis.eu>
2023-07-09 22:21:11 +0200

    Fichiers volumineux finito

--- README.md
+++ README.md
@@ -2,17 +2,3 @@
 # Author: Margot Berton
 
 Objective: Automatically generate a subtitle file from an uploaded video (training: natural language processing).
-
-
-##Sprint 1:
-- Minimalist interface (it will remain local) to: (i) upload the video, (ii) click a button to generate the subtitle file, (iii) download the subtitle file.
-- Only one language: English.
-- No manual modifications.
-
-##Sprint 2:
-- Multiple languages: French and Spanish.
-- Include a language selection step in the interface.
-
-##Sprint 3:
-- Allow manual modifications.
-- Include a (basic) dashboard that allows choosing where to place the subtitles.
---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#Libraries
+from moviepy.editor import VideoFileClip
+import os
+import torch
+
+
+#Configuration
+torch.set_num_threads(1)
+model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad', model='silero_vad')
+(get_speech_timestamps, _, read_audio, _, _) = utils
+
+
+#Variables
+video_file = "/home/margot/Portfolio/subtitling-app/Test/Dune Official Trailer.mp4"
+
+
+#Functions
+def extract_audio(video_file, audio_file):
+    video = VideoFileClip(video_file)
+    audio = video.audio
+    audio.write_audiofile(audio_file)
+    video.close()
+
+def vad(audio_file):
+    wav = read_audio(audio_file, sampling_rate=16000)
+    speech_timestamps = get_speech_timestamps(wav, model, sampling_rate=16000, visualize_probs=True, return_seconds=True)
+    print(speech_timestamps)
+
+
+#Main function
+def main(video_file):
+    base_path = os.path.splitext(video_file)[0]
+    audio_file = base_path + ".wav"
+    str_file = base_path + ".str"
+
+    extract_audio(video_file, audio_file)
+    vad(audio_file)
+
+    os.remove(audio_file)
+
+
+main(video_file)
