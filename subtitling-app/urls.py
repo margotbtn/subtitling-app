24f74192cef5c1bdb612eac0d8167cde2642f0c9
margot BERTON <margot.berton@telecom-sudparis.eu>
2023-08-28 01:45:04 +0200

    Retirer /index/

--- subtitling-app/urls.py
+++ subtitling-app/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('about-me/', views.about, name='about-me'),
 ]
 
